f255c58661f3ef5f6561610c4b4fe8f64dff9667
aki7 <owlstemple@live.jp>
2019-08-18 06:15:43 +0000

    toppage

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,22 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">料理ガチャ</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <form class="form-inline">
+                <input class="form-control mr-1" type="text" name=""/>
+                <button class="btn btn-primary mr-2" type="submit">検索</button>
+            </form>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">登録</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ユーザ登録</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
